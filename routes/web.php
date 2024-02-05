c7656facedb96fb36ba4882979da7052591c5c8e
Ioannis Stratakis <jstratakis92@gmail.com>
2024-02-05 20:04:16 +0200

    Add docker and authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\UrlController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,19 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('auth.login');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-// route for urls
-Route::resource('urls', UrlController::class)
-->middleware(['auth', 'verified']);
-
-// route for get shortener url
-Route::get('{shortened_url}', [UrlController::class, 'shortenLink'])->name('shortened-url'); 
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
